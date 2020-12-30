8476ad786902ac6966dc6ccf408d571e83c2bb5e
Mark Radbourne <markrad@msn.com>
2020-12-30 12:18:29 -0800

    Add support for dimmers and test

--- apps/switchtest.js
+++ apps/switchtest.js
@@ -5,43 +5,53 @@ var logger = log4js.getLogger(CATEGORY);
 
 class SwitchTest {
     constructor(controller, config) {
-        this.testtoggle = controller.items.testtoggle;
-        this.bob = controller.items.bob;
+        // this.testtoggle = controller.items.testtoggle;
+        // this.bob = controller.items.bob;
+        this.patio = controller.items.jasco_products_14294_in_wall_smart_dimmer_level;
         this.interval = null;
         logger.debug('Constructed');
     }
 
     async run() {
         let tests = [
-            async () => this.bob.updateState(0),
-            async () => this.bob.updateState(10),
-            async () => this.bob.updateState(20),
-            async () => this.bob.updateState(30),
-            async () => this.bob.updateState(40),
-            async () => this.bob.updateState(50),
-            async () => this.bob.updateState(50),
-            async () => this.bob.updateState(50),
-            async () => this.bob.updateState(150),
-            async () => this.bob.incrementState(),
-            async () => this.bob.decrementState(),
-            async () => this.testtoggle.turnOn(),
-            async () => this.testtoggle.turnOff(),
-            async () => this.testtoggle.toggle(),
-            async () => this.testtoggle.toggle(),
-            async () => this.testtoggle.turnOn(),
-            async () => this.testtoggle.turnOff(),
-            async () => this.testtoggle.toggle(),
-            async () => this.testtoggle.toggle(),
+            // async () => this.bob.updateState(0),
+            // async () => this.bob.updateState(10),
+            // async () => this.bob.updateState(20),
+            // async () => this.bob.updateState(30),
+            // async () => this.bob.updateState(40),
+            // async () => this.bob.updateState(50),
+            // async () => this.bob.updateState(50),
+            // async () => this.bob.updateState(50),
+            // async () => this.bob.updateState(150),
+            // async () => this.bob.incrementState(),
+            // async () => this.bob.decrementState(),
+            // async () => this.testtoggle.turnOn(),
+            // async () => this.testtoggle.turnOff(),
+            // async () => this.testtoggle.toggle(),
+            // async () => this.testtoggle.toggle(),
+            // async () => this.testtoggle.turnOn(),
+            // async () => this.testtoggle.turnOff(),
+            // async () => this.testtoggle.toggle(),
+            // async () => this.testtoggle.toggle(),
+            async () => this.patio.turnOff(),
+            async () => this.patio.updateState(20),
+            async () => this.patio.updateState(40),
+            async () => this.patio.updateState(60),
+            async () => this.patio.updateState(80),
+            async () => this.patio.updateState(100),
+            async () => this.patio.turnOff(),
         ];
+        //return;
         let counter = 0;
 
         this.interval = setInterval(async () => {
-            logger.info(`Current state testtoggle=${this.testtoggle.state} bob=${this.bob.state}`);
-            await tests[counter++]();
-            logger.info(`New state testtoggle=${this.testtoggle.state} bob=${this.bob.state}`);
-
-            if (counter == tests.length) {
+            if (counter >= tests.length) {
                 clearInterval(this.interval);
+            }
+            else {
+                // logger.info(`Current state testtoggle=${this.testtoggle.state} bob=${this.bob.state}`);
+                await tests[counter++]();
+                // logger.info(`New state testtoggle=${this.testtoggle.state} bob=${this.bob.state}`);
             }
         }, 2 * 1000);
     }
--- haitems/haitemlight.js
+++ haitems/haitemlight.js
@@ -8,15 +8,34 @@ class HaItemLight extends HaGenericSwitchItem {
         this.logger = log4js.getLogger(this.category);
         this.logger.level = 'debug';
         this.on('new_state', (that, _oldstate) => {
-            this.logger.debug(`Received new state: ${that.state}`);
+            this.logger.debug(`Received new state: ${that.state} ${that.attributes.brightness? 'Brightness: ' + that.attributes.brightness : ''}`);
         });
     }
 
     async updateState(newState) {
         return new Promise((resolve, _reject) => {
-            var { action, expectedNewState } = this._getActionAndExpectedSNewtate(newState);
-            this._callServicePromise(resolve, newState, expectedNewState, this.type, action, { entity_id: this.entityId });
+            let { action, expectedNewState } = this._getActionAndExpectedSNewtate(newState);
+            let brightness = Number(newState);
+            let set = { entity_id: this.entityId };
+
+            if (action == 'turn_on' && NaN != brightness) {
+                brightness == brightness > 100
+                    ? 100
+                    : brightness < 1
+                    ? 1
+                    : brightness;
+                brightness = 255 / 100 * brightness
+                set['brightness'] = brightness;
+            }
+
+            this._callServicePromise(resolve, newState, expectedNewState, this.type, action, set);
         });
+    }
+
+    _childOveride(set) {
+        // HA will sometimes round slightly differently so a change of one point is not seen as a change and not send the update
+        // If the brightness change is less than four points it will be rejected as already in that state.
+        return !!set.brightness && Math.abs(Number(set.brightness) - Number(this.attributes.brightness)) > 3;
     }
 }
 
