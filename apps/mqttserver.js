86c6509c78d150adc295ee6930b8478d632bff67
Mark Radbourne <markrad@msn.com>
2021-01-08 15:01:47 -0800

    Track clients and expose them in the API

--- apps/mqttserver.js
+++ apps/mqttserver.js
@@ -6,38 +6,52 @@ const logger = require('log4js').getLogger(CATEGORY);
 class MqttServer extends EventEmitter {
     constructor(_controller, _config) {
         super();
-        this.aedes = require('aedes')();
-        this.port = 1883;
-        this.server = require('net').createServer(this.aedes.handle);
+        this._aedes = require('aedes')();
+        this._port = 1883;
+        this._server = require('net').createServer(this._aedes.handle);
+        this._clients = [];
         logger.info('Construction complete');
     }
 
     run() {
-        this.server.listen(this.port, () => { 
-            this.aedes.on('client', (client) => logger.debug(`New client ${client.id}`));
-            this.aedes.on('clientReader', (client) => logger.debug(`Client ready ${client.id}`));
-            this.aedes.on('clientDisconnect', (client) => logger.debug(`Client disconnected ${client.id}`));
-            this.aedes.on('clientError', (client, err) => logger.debug(`Client errored ${client.id} - ${err.message}`));
-            this.aedes.on('connectionError', (client, err) => logger.debug(`Connection errored ${client.id} - ${err.message}`));
-            this.aedes.on('publish', (packet, client) => {
+        this._server.listen(this._port, () => { 
+            this._aedes.on('client', (client) => {
+                this._clients.push(client);
+                logger.debug(`New client ${client.id}`)
+            });
+            this._aedes.on('clientReader', (client) => logger.debug(`Client ready ${client.id}`));
+            this._aedes.on('clientDisconnect', (client) => {
+                this._clients = this._clients.filter(value => value != client);
+                logger.debug(`Client disconnected ${client.id}`)
+            });
+            this._aedes.on('clientError', (client, err) => {
+                this._clients = this._clients.filter(value => value != client);
+                logger.debug(`Client errored ${client.id} - ${err.message}`)
+            });
+            this._aedes.on('connectionError', (client, err) => logger.debug(`Connection errored ${client.id} - ${err.message}`));
+            this._aedes.on('publish', (packet, client) => {
                 logger.debug(`Publish ${client != null? client.id : 'none'} topic=${packet.topic} payload=${packet.payload.toString()}`);
             });
-            this.aedes.on('subscribe', (options, client) => {
+            this._aedes.on('subscribe', (options, client) => {
                 let topics = '';
                 options.forEach((topic) => topics += (topic.topic + ' '));
                 logger.debug(`Subscribe ${client.id} topics=${topics}`);
             });
-            logger.info('MQTT server started and listening on port ' + this.port)
+            logger.info('MQTT server started and listening on port ' + this._port)
         });
     }
 
     async stop() {
         return new Promise((resolve, _reject) => {
-            this.aedes.close(() => { 
+            this._aedes.close(() => { 
                 logger.info('MQTT server stopped');
                 resolve();
             });
         });
+    }
+
+    get clients() {
+        return this._clients;
     }
 }
 
