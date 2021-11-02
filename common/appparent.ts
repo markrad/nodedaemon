13719bef47cf1dc09313404181356204ada1ebff
Mark Radbourne <markrad@msn.com>
2021-11-02 16:02:56 -0700

    :art: Add an app parent class for common functionality

---
+++ common/appparent.ts
@@ -0,0 +1,30 @@
+import { Logger } from "log4js";
+import { IApplication } from "./IApplication";
+import { LogLevelValidator } from './loglevelvalidator';
+import { EventEmitter } from 'events';
+
+export abstract class AppParent extends EventEmitter implements IApplication {
+    private _logger: Logger;
+    constructor(logger: Logger) {
+        super();
+        this._logger = logger;
+    }
+    abstract validate(config: any): boolean;
+    abstract run(): Promise<boolean>;
+    abstract stop(): void;
+
+    public get logging(): string {
+        return this._logger.level;
+    }
+
+    public set logging(value: string) {
+        if (!LogLevelValidator(value)) {
+            let err: Error = new Error(`Invalid level passed: ${value}`);
+            this._logger.error(err.message);
+            throw err;
+        }
+        else {
+            this._logger.level = value;
+        }
+    }
+}
