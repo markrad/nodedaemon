cd2512f4f8d6a67420b16cc9afeb482210a0c43d
Mark Radbourne <markrad@msn.com>
2022-01-15 17:07:33 -0800

    :bug: Issue with new yaml configuration file

--- sysapps/motionlight/index.ts
+++ sysapps/motionlight/index.ts
@@ -6,6 +6,7 @@ import { HaMain } from "../../hamain";
 import { State } from '../../hamain/state';
 import { getLogger, Logger } from 'log4js';
 import { AppParent } from '../../common/appparent';
+import * as HaItemBinarySensor from "../../haitems/haitembinary_sensor";
 
 interface Trip {
     sensor: IHaItem;
@@ -27,54 +28,64 @@ export default class MotionLight extends AppParent {
     }
 
     public validate(config: any): boolean {
-        if (!config.devices) {
+        if (!config) {
             logger.error('No devices specified');
         }
         else {
-            if (!Array.isArray(config.devices)) {
+            if (!Array.isArray(config)) {
                 logger.error('Invalid device specification');
                 return false;
             }
-            this._trips = config.devices.map((value: any[]) => {
-                if (!Array.isArray(value)) {
-                    logger.error(`Specified value is not an array: ${value}`);
+            this._trips = config.map((value: any) => {
+                if (value.sensor == undefined) {
+                    logger.error('Entry does not contain a sensor');
+                    return null
                 }
-                if (!Array.isArray(value[1])) {
-                    value[1] = [ value[1] ];
+                if (value.switch == undefined) {
+                    logger.error('Entry does not contain any switches');
+                    return null;
                 }
-                if (!this._controller.items.getItem(value[0])) {
-                    logger.error(`Specified motion sensor does not exist: ${value[0]}`);
+                if (value.delay == undefined) {
+                    logger.error('Entry does not contain a delay');
+                    return null;
                 }
-                else if (this._controller.items.getItem(value[0]).type != 'binary_sensor') {
-                    logger.error(`Specified motion sensor needs to be type binary_sensor: ${value[0]}`)
+                if (typeof value.delay != 'number') {
+                    logger.error('Delay must be a number');
+                    return null;
                 }
-                else if (false == value[1].reduce((flag: boolean, value: string) => {
+                if (null == this._controller.items.getItemAs((HaItemBinarySensor as any).default, value.sensor)) {
+                    logger.error(`Sensor ${value.sensor} is either missing or not a binary_sensor type`);
+                    return null;
+                }
+                if (!Array.isArray(value.switch)) {
+                    value.switch = [ value.switch ];
+                }
+//----
+                if (false == value.switch.reduce((flag: boolean, value: string) => {
                     if (!this._controller.items.getItem(value)) {
                         logger.error(`Specified target light does not exist: ${value}`);
                         flag = false;
                     }
                     else if (!this._controller.items.getItem(value).isSwitch) {
-                        logger.error(`Specified target light is not a switch or a light: ${value[1]}`);
+                        logger.error(`Specified target light is not a switch or a light: ${value}`);
                         flag = false;
                     }
 
                     return flag;
                 }, true)) {
                     logger.error(`Invalid lights found in target array`);
-                }
-                else if (typeof(value[2]) != 'number') {
-                    logger.error(`Minutes before action is not numeric`);
+                    return null;
                 }
                 else {
-                    let lights: IHaItemSwitch[] = value[1].map((value: string) => this._controller.items.getItem(value));
+                    let lights: IHaItemSwitch[] = value.switch.map((value: string) => this._controller.items.getItem(value));
                     let trip: Trip = { 
-                        sensor: this._controller.items.getItem(value[0]),
+                        sensor: this._controller.items.getItem(value.sensor),
                         lights: lights,
-                        timeout: value[2]
+                        timeout: value.delay
                     };
                     return trip;
                 }
-            }).filter((item: Trip) => item != undefined);
+            }).filter((item: Trip) => item != null);
             logger.info('Validated successfully');
 
             return true;
@@ -136,33 +147,6 @@ class actioner {
         }
         this._trip.sensor.on('new_state', this._eventHandler); 
     }
-
-    // private _eventHandler(that: IHaItem, _oldState: State): void {
-    //     logger.debug(`State ${that.state} triggered on ${this._trip.sensor.entityId} for ${this._trip.lights.map(item => item.entityId).join(' ')}`);
-    //     if (that.state == 'on') {
-    //         this._trip.lights.forEach((light: IHaItemSwitch) => {
-    //             light.turnOffAt(Date.now() + this._trip.timeout * 60 * 1000);
-    //         });
-    //         this._timer = setInterval(() => 
-    //         { 
-    //             if (!this._trip.lights[0].isTimerRunning) {
-    //                 clearInterval(this._timer);
-    //                 this._timer = null;
-    //             }
-    //             else if (this._trip.sensor.state == 'on') {
-    //                 logger.trace(`Checking motion sensor status: ${this._trip.sensor.state}`);
-    //                 this._trip.lights.forEach((light) => {
-    //                     logger.debug('Extending turn off time');
-    //                     light.turnOffAt(Date.now() + this._trip.timeout * 60 * 1000);
-    //                 });
-    //             }
-    //         }, 30000);
-    //     }
-    //     else {
-    //         clearInterval(this._timer);
-    //         this._timer = null;
-    //     }
-    // }
 
     public async stop(): Promise<void> {
         this._trip.sensor.off('new_state', this._eventHandler);
