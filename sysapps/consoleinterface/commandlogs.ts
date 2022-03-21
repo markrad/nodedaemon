9c8a50475f7db86649d572df1aca4f1c6de01d4d
Mark Radbourne <markrad@msn.com>
2022-03-21 10:37:13 -0700

    :sparkles: Add regex to logs command

--- sysapps/consoleinterface/commandlogs.ts
+++ sysapps/consoleinterface/commandlogs.ts
@@ -1,43 +1,55 @@
 import ConsoleInterface from ".";
 import { IChannel } from "./ichannel";
 import { CommandBase } from "./commandbase";
-// import { getLogger, Logger } from "log4js";
+import { getLogger, Logger } from "log4js";
 import { logEmitter } from "../../common/emitlogger";
 import { EventWaiter } from "../../common/eventwaiter";
 // import { EventWaiter } from "../../common/eventwaiter";
 
-// const CATEGORY: string = 'CommandLogs';
-// var logger: Logger = getLogger(CATEGORY);
+const CATEGORY: string = 'CommandLogs';
+var logger: Logger = getLogger(CATEGORY);
 
 export class CommandLogs extends CommandBase {
     private _isRunning: boolean = false;
     private _sock: IChannel = null;
+    private _regex: RegExp = null;
     private _ew: EventWaiter = null;
-    private _messageWriter = (message:string) => { if (this._sock) this._sock.write(`${message}\r\n`); }
+    private _messageWriter = (message:string) => { 
+        if (this._regex == null || this._regex.exec(message) != null) {
+            if (this._sock) this._sock.write(`${message}\r\n`); 
+        }
+    }
     public constructor() {
         super('logs');
     }
 
     public get helpText(): string {
-        return `${this.commandName}\t\t\t\tFollow logs`;
+        return `${this.commandName}\t<Optional Regex>\tFollow logs with optional filtering`;
     }
 
-    public async execute(_inputArray: string[], _that: ConsoleInterface, sock: IChannel): Promise<void> {
+    public async execute(inputArray: string[], _that: ConsoleInterface, sock: IChannel): Promise<void> {
         return new Promise((resolve, reject) => {
             // this._messageWriter = (message:string) => { if (this._sock) this._sock.write(`${message}\r\n`); }
             try {
+                if (inputArray.length > 2) throw new Error('Only one optional argument is permitted')
+                this._sock = sock;
+                if (inputArray.length == 2) this._regex = new RegExp(inputArray[1]);
+                console.log(this._regex);
                 logEmitter.on('logmessage', this._messageWriter);
                 this._isRunning = true;
-                this._sock = sock;
                 this._ew = new EventWaiter();
                 this._ew.EventWait().then(() => resolve());
             }
             catch (err) {
                 this._isRunning = false;
+                logger.error(err.message);
                 sock.write(`${err}\r\n`);
+                sock.write('Usage:\r\n');
+                sock.write(this.helpText);
+                sock.write('\r\n');
                 reject(err);
             }
-            });
+        });
     }
 
     public async terminate(_that: ConsoleInterface, _sock: IChannel): Promise<void> {
