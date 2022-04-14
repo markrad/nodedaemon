605acbc569c844d324dd58f7c58377b1d0184635
Mark Radbourne <markrad@msn.com>
2022-04-14 15:09:36 -0700

    :children_crossing: Log unknown entity value when encountered

--- haitems/haitemunknown.ts
+++ haitems/haitemunknown.ts
@@ -4,7 +4,7 @@ import { HaParentItem } from './haparentitem';
 class HaItemUnknown extends HaParentItem {
     public constructor(item: State) {
         super(item);
-        this.logger.warn('Unknown entity');
+        this.logger.warn(`Unknown entity: ${item.entity_id}`);
         this.on('new_state', (that, _oldstate) => {
             this.logger.debug(`Received new state: ${that.state}`);
         });
