54f4fa2e20918bab816defe6457416ccce7ec5c0
Mark Radbourne <markrad@msn.com>
2022-04-25 14:51:27 -0700

    :art: Create generic button parent

---
+++ haitems/hagenericbuttonitem.ts
@@ -0,0 +1,21 @@
+import { State } from '../hamain/state'
+import { HaGenericSwitchItem } from './hagenericswitchitem';
+import { ServicePromise } from './haparentitem';
+
+export abstract class HaGenericButtonItem extends HaGenericSwitchItem {
+    public constructor(item: State, logLevel?: string) {
+        super(item, logLevel);
+    }
+
+    public async press(): Promise<ServicePromise> {
+        return new Promise<ServicePromise>(async (resolve, _reject) => {
+            let ret: ServicePromise = null;
+            ret = await this.updateState('press');
+            resolve(ret);
+        });
+    }
+
+    public get isButton() {
+        return true;
+    }
+}
--- haitems/haiteminput_button.ts
+++ haitems/haiteminput_button.ts
@@ -1,10 +1,10 @@
 import { State } from '../hamain/state'
 import { HaGenericButtonItem } from './hagenericbuttonitem';
 
-export class HaItemButton extends HaGenericButtonItem {
+export class HaItemInputButton extends HaGenericButtonItem {
     public constructor(item: State, logLevel?: string) {
         super(item, logLevel);
     }
 }
 
-module.exports = HaItemButton;
+module.exports = HaItemInputButton;
