de2b2fb62cbf35fdee2641fc977d0579166dcd53
Mark Radbourne <markrad@msn.com>
2022-09-23 14:50:29 -0700

    :white_check_mark: Junk test code

--- sysapps/testbed/index.ts
+++ sysapps/testbed/index.ts
@@ -1,9 +1,11 @@
 import { getLogger, Logger } from 'log4js';
 import { AppParent } from '../../common/appparent';
-import { HaMain, SensorType } from '../../hamain';
+import { HaMain /*, SensorType */ } from '../../hamain';
 // import { HaItemSensor } from '../../haitems/haitemsensor'
-import { IHaItemEditable } from '../../haitems/haparentitem';
-import { HaGenericSwitchItem } from '../../haitems/hagenericswitchitem';
+// import { IHaItemEditable } from '../../haitems/haparentitem';
+// import { HaGenericSwitchItem } from '../../haitems/hagenericswitchitem';
+// import { resolve } from 'path';
+import { HaItemUpdate } from '../../haitems/haitemupdate';
 
 const CATEGORY: string = 'TestBed';
 var logger: Logger = getLogger(CATEGORY);
@@ -16,25 +18,30 @@ export default class TestBed extends AppParent {
 
     validate(_config: any): boolean {
         // Since this is test code we don't do much in here
-        let a: HaGenericSwitchItem = this.controller.items.getItemAs<HaGenericSwitchItem>(HaGenericSwitchItem, 'light.marks_light');
-        let b: HaGenericSwitchItem = this.controller.items.getItemAs<HaGenericSwitchItem>(HaGenericSwitchItem, 'sensor.bedroom_nightstand_1_battery_level');
-        logger.debug(a);
-        logger.debug(b);
-        logger.info('Validated successfully')
+        let x: HaItemUpdate = (this.controller.items.getItem('update.rr_synol1_dsm_update') as HaItemUpdate);
+        logger.debug(x.state);
+        logger.debug(`isOn=${x.isOn}; isOff=${x.isOff}`);
+        // let a: HaGenericSwitchItem = this.controller.items.getItemAs<HaGenericSwitchItem>(HaGenericSwitchItem, 'light.marks_light');
+        // let b: HaGenericSwitchItem = this.controller.items.getItemAs<HaGenericSwitchItem>(HaGenericSwitchItem, 'sensor.bedroom_nightstand_1_battery_level');
+        // logger.debug(a);
+        // logger.debug(b);
+        // logger.info('Validated successfully')
         return true;
     }
 
     async run(): Promise<boolean> {
         return new Promise(async (resolve, _reject) => {
-            let name: string = 'testbed_' + Math.floor(Math.random() * 10000).toString();
-            try {
-                await this.controller.addSensor(name, SensorType.normal, 'testbed1');
-                ((this.controller.items.getItem(`sensor.${name}`)) as IHaItemEditable).updateState('testbed2'); 
-                resolve(true);
-            }
-            catch (err) {
-                logger.error(`Failed to add sensor ${name}: ${err.message}`)
-            }
+        //     let name: string = 'testbed_' + Math.floor(Math.random() * 10000).toString();
+        //     try {
+        //         await this.controller.addSensor(name, SensorType.normal, 'testbed1');
+        //         ((this.controller.items.getItem(`sensor.${name}`)) as IHaItemEditable).updateState('testbed2'); 
+        //         resolve(true);
+        //     }
+        //     catch (err) {
+        //         logger.error(`Failed to add sensor ${name}: ${err.message}`)
+        //     }
+        // });
+            resolve(true);
         });
     }
 
