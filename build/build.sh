a04fcc39e297022683ee3331a676ba3edd1b3458
Mark Radbourne <markrad@msn.com>
2023-01-04 18:28:34 -0800

    :sparkles: Calculate next version automatically

--- build/build.sh
+++ build/build.sh
@@ -1,17 +1,50 @@
-if [ $# -ne 1 ]
+if [ $# -eq 0 ]
 then
-    echo You must pass a new version number
+    field=2
+elif [ $# -ne 1 ]
+then
+    echo Invalid parameters
+    echo Usage:
+    echo "   build.sh [-patch | -minor | -major ] - defaults to -patch"
     exit 4
+else
+    if [ $1 == "-patch" ]
+    then
+        field=2
+    elif [ $1 == "-minor" ]
+    then
+        field=1
+    elif [ $1 == "-major" ]
+    then
+        field=0
+    else
+        echo Invalid parameters
+        echo Usage:
+        echo "   build.sh [-patch | -minor | -major ] - defaults to -patch"
+        exit 4
+    fi
 fi
 
 version=$(jq .version ../package.json | tr -d '"')
+IFS="." read -a parts <<< $version
+echo ${parts[$field]}
+parts[$field]=$((${parts[$field]}+1))
+echo ${parts[$field]}
+newver=$(IFS="." ; echo "${parts[*]}")
+echo $newver
 
-echo This will update the version from $version to $1 and push a new tag
+echo This will update the version from $version to $newver and push a new tag
 read -p "Do you wish to continue? [Yy] " response
 
 if [ $response != "Y" ] && [ $response != "y" ]
 then
     echo Exiting
+    exit 4
+fi
+
+if [ 1 -ne $(grep -c $1 Dockerfile) ] || [ 1 -ne $(grep -c $1 ../package.json) ]
+then
+    echo Version $1 occurs more than once in one of the build files
     exit 4
 fi
 
