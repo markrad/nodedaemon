ba8f62e4475c0d4a48172292ca408500bc191da6
Mark Radbourne <markrad@msn.com>
2023-03-19 19:27:57 -0700

    :bug: Fix broken start

--- tools/keepalive/index.ts
+++ tools/keepalive/index.ts
@@ -1,6 +1,17 @@
 import { KeepAlive } from "./keepalive";
-import { getLogger, Logger } from "log4js";
+import { getLogger, Logger, Configuration, configure } from "log4js";
 
+
+let loggerOptions: Configuration = {
+    appenders: {
+        out: { type: 'stdout' },
+    },
+    categories: {
+        default: { appenders: ['out'], level: 'debug' }
+    }
+}
+
+configure(loggerOptions);
 let logger: Logger = getLogger();
 
 new KeepAlive(process.argv.slice(2), logger).start();
