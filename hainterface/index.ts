c438a22a9c5b2728fa41f6ab4f31e9e5c56c35f4
Mark Radbourne <markrad@msn.com>
2023-04-06 09:21:58 -0700

    :art: Remove unnecessary toString call

--- hainterface/index.ts
+++ hainterface/index.ts
@@ -131,7 +131,7 @@ export class HaInterface extends EventEmitter {
     public async start(): Promise<void> {
         return new Promise<void>(async (resolve, reject): Promise<void> => {    
             try {
-                this._client = new WSWrapper(`${this._protocol}${this._hostname}:${this._port}${HaInterface.APIPATH}`.toString(), this._pingInterval, logger.level as Level)
+                this._client = new WSWrapper(`${this._protocol}${this._hostname}:${this._port}${HaInterface.APIPATH}`, this._pingInterval, logger.level as Level);
                 logger.info(`Connecting to ${this._client.url}`);
 
                 this._client.on('message', async (message: string) => {
